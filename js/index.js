73f23a927bb738dbb05685ed72da56256eb51cf6
Theresa <tmannschatz@gmail.com>
2023-03-29 22:59:33 +0200

    Try: enabling mobile language select

--- js/index.js
+++ js/index.js
@@ -4,21 +4,21 @@ function translate(lng, tagAttr){
   translate.init(tagAttr, lng);
   translate.process();
   if(lng == 'en'){
-    $("#enTranslator").css('color', '#8B0000');
-    $("#deTranslator").css('color', '#DE9C21');
+    $(".enTranslator").css('color', '#8B0000');
+    $(".deTranslator").css('color', '#DE9C21');
   }
   if(lng == 'de'){
-    $("#deTranslator").css('color', '#8B0000');
-    $("#enTranslator").css('color', '#DE9C21');
+    $(".deTranslator").css('color', '#8B0000');
+    $(".enTranslator").css('color', '#DE9C21');
   }
 }
 $(document).ready(function(){
   //This is id of HTML element (English) with attribute lng-tag
-  $("#enTranslator").click(function(){
+  $(".enTranslator").click(function(){
     translate('en', 'lng-tag');
   });
   //This is id of HTML element (German) with attribute lng-tag
-  $("#deTranslator").click(function(){
+  $(".deTranslator").click(function(){
     translate('de', 'lng-tag');
   });
 });
