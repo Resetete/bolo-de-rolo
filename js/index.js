35c6a8046c59b6176a98677e1f3d9122123e7847
Theresa <tmannschatz@gmail.com>
2023-03-30 21:45:16 +0200

    Store choosen language in cookie

--- js/index.js
+++ js/index.js
@@ -12,13 +12,33 @@ function translate(lng, tagAttr){
     $(".enTranslator").css('color', 'black');
   }
 }
+
+// retrieve cookies
+function getCookie(cName) {
+  const name = cName + "=";
+  const cDecoded = decodeURIComponent(document.cookie); //to be careful
+  const cArr = cDecoded .split('; ');
+  let res;
+  cArr.forEach(val => {
+      if (val.indexOf(name) === 0) res = val.substring(name.length);
+  })
+  return res;
+}
+
 $(document).ready(function(){
   //This is id of HTML element (English) with attribute lng-tag
+  if (getCookie('language')) {
+    cookieLang = getCookie('language');
+    translate(cookieLang, 'lng-tag')
+  };
+
   $(".enTranslator").click(function(){
+    document.cookie = 'language=en'; // set the cookie with the language if user clicks the language button
     translate('en', 'lng-tag');
   });
   //This is id of HTML element (German) with attribute lng-tag
   $(".deTranslator").click(function(){
+    document.cookie = 'language=de'; // set the cookie with the language if user clicks the language button
     translate('de', 'lng-tag');
   });
 });
